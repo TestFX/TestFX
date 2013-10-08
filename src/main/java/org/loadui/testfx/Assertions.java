454c4f2583dc5b9b5bed700bebd5be395002577f
Henrik Olsson <henriko491@gmail.com>
2013-10-08 11:54:13 +0200

    LOADUI-1031

--- src/main/java/org/loadui/testfx/Assertions.java
+++ src/main/java/org/loadui/testfx/Assertions.java
@@ -3,20 +3,15 @@ package org.loadui.testfx;
 import javafx.scene.Node;
 import org.hamcrest.Matcher;
 
-import static org.loadui.testfx.GuiTest.find;
-import static org.junit.Assert.fail;
-
 public class Assertions
 {
 	public static void assertNodeExists( Matcher<?> matcher )
 	{
-		if( GuiTest.find( (Matcher<Node>) matcher ) == null )
-			fail("No node matches " + matcher);
+		GuiTest.find( ( Matcher<Node> )matcher );
 	}
 
 	public static void assertNodeExists( String query )
 	{
-		if( GuiTest.find( query ) == null )
-			fail("No node matches '" + query +"'" );
+		GuiTest.find( query );
 	}
 }
