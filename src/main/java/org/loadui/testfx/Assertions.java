07a3adcb7a59ce7ee510d7d27aaac3436d9c130c
Maximilian Skog <maximilian.skog@smartbear.com>
2013-10-31 11:05:06 +0100

    Added verifyThat assertion with reason

--- src/main/java/org/loadui/testfx/Assertions.java
+++ src/main/java/org/loadui/testfx/Assertions.java
@@ -6,8 +6,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Assertions
 {
-	@SuppressWarnings("unchecked")
-    public static void assertNodeExists( Matcher<?> matcher )
+	@SuppressWarnings( "unchecked" )
+	public static void assertNodeExists( Matcher<?> matcher )
 	{
 		GuiTest.find( ( Matcher<Object> )matcher );
 	}
@@ -17,12 +17,18 @@ public class Assertions
 		GuiTest.find( query );
 	}
 
-	public static <T> void verifyThat(T value, Matcher<? super T> matcher)
+	public static <T> void verifyThat( T value, Matcher<? super T> matcher )
+	{
+		verifyThat( "", value, matcher );
+	}
+
+	public static <T> void verifyThat( String reason, T value, Matcher<? super T> matcher )
 	{
 		try
 		{
-			assertThat( value, matcher );
-		} catch( AssertionError e )
+			assertThat( reason, value, matcher );
+		}
+		catch( AssertionError e )
 		{
 			throw new AssertionError( e.getMessage() + " Screenshot saved as " + GuiTest.captureScreenshot().getAbsolutePath() , e );
 		}
