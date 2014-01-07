e040ecb24e59667d8a1ae3d00341aaf4cd0ed288
Henrik Olsson <henrik.olsson@smartbear.com>
2014-01-07 22:06:41 +0100

    Fixed odd drag-n-drop bug #33

--- src/main/java/org/loadui/testfx/FXScreenController.java
+++ src/main/java/org/loadui/testfx/FXScreenController.java
@@ -1,168 +1,142 @@
-/*
- * Copyright 2013 SmartBear Software
- * 
- * Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the European Commission - subsequent
- * versions of the EUPL (the "Licence");
- * You may not use this work except in compliance with the Licence.
- * You may obtain a copy of the Licence at:
- * 
- * http://ec.europa.eu/idabc/eupl
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the Licence is
- * distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the Licence for the specific language governing permissions and limitations
- * under the Licence.
- */
 package org.loadui.testfx;
 
 import com.google.common.collect.ImmutableMap;
-import javafx.animation.Interpolator;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
-import javafx.application.Platform;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
-import javafx.util.Duration;
 import org.loadui.testfx.utils.FXTestUtils;
 
 import java.awt.*;
 import java.awt.event.InputEvent;
 import java.util.Map;
-import java.util.concurrent.CountDownLatch;
 
 public class FXScreenController implements ScreenController
 {
-	private static final Map<MouseButton, Integer> BUTTONS = ImmutableMap.of( MouseButton.PRIMARY,
-			InputEvent.BUTTON1_MASK, MouseButton.MIDDLE, InputEvent.BUTTON2_MASK, MouseButton.SECONDARY,
-			InputEvent.BUTTON3_MASK );
+    private static final Map<MouseButton, Integer> BUTTONS = ImmutableMap.of( MouseButton.PRIMARY,
+            InputEvent.BUTTON1_MASK, MouseButton.MIDDLE, InputEvent.BUTTON2_MASK, MouseButton.SECONDARY,
+            InputEvent.BUTTON3_MASK );
 
-	private final DoubleProperty mouseXProperty = new SimpleDoubleProperty();
-	private final DoubleProperty mouseYProperty = new SimpleDoubleProperty();
-	private final Robot robot;
-	private long moveTime = 175;
+    private final DoubleProperty mouseXProperty = new SimpleDoubleProperty();
+    private final DoubleProperty mouseYProperty = new SimpleDoubleProperty();
+    private final Robot robot;
+    private long moveTime = 175;
 
-	public FXScreenController()
-	{
-		try
-		{
-			robot = new Robot();
-		}
-		catch( AWTException e )
-		{
-			throw new IllegalArgumentException( e );
-		}
+    public FXScreenController()
+    {
+        try
+        {
+            robot = new Robot();
+        }
+        catch( AWTException e )
+        {
+            throw new IllegalArgumentException( e );
+        }
 
-		final ChangeListener<Number> mouseChangeListener = new ChangeListener<Number>()
-		{
-			@Override
-			public void changed( ObservableValue<? extends Number> value, Number oldNum, Number newNum )
-			{
-				robot.mouseMove( mouseXProperty.intValue(), mouseYProperty.intValue() );
-			}
-		};
+        final ChangeListener<Number> mouseChangeListener = new ChangeListener<Number>()
+        {
+            @Override
+            public void changed( ObservableValue<? extends Number> value, Number oldNum, Number newNum )
+            {
+                robot.mouseMove( mouseXProperty.intValue(), mouseYProperty.intValue() );
+                System.out.println("!");
+            }
+        };
 
-		mouseXProperty.addListener( mouseChangeListener );
-		mouseYProperty.addListener( mouseChangeListener );
-	}
+        mouseXProperty.addListener( mouseChangeListener );
+        mouseYProperty.addListener( mouseChangeListener );
+    }
 
-	@Override
-	public Point2D getMouse()
-	{
-		return new Point2D( mouseXProperty.doubleValue(), mouseYProperty.doubleValue() );
-	}
+    @Override
+    public Point2D getMouse()
+    {
+        return new Point2D( mouseXProperty.doubleValue(), mouseYProperty.doubleValue() );
+    }
 
-	@Override
-	public void position( double x, double y )
-	{
-		mouseXProperty.set( x );
-		mouseYProperty.set( y );
-	}
+    @Override
+    public void position( double x, double y )
+    {
+        mouseXProperty.set( x );
+        mouseYProperty.set( y );
+    }
 
-	@Override
-	public void move( final double x, final double y )
-	{
-		Point currentMousePosition = MouseInfo.getPointerInfo().getLocation();
-		mouseXProperty.set( currentMousePosition.getX() );
-		mouseYProperty.set( currentMousePosition.getY() );
+    @Override
+    public void move( final double x, final double y )
+    {
+        Point currentMousePosition = MouseInfo.getPointerInfo().getLocation();
+        mouseXProperty.set( currentMousePosition.getX() );
+        mouseYProperty.set( currentMousePosition.getY() );
 
-		final CountDownLatch done = new CountDownLatch( 1 );
+        boolean reachedTarget = false;
+        while(!reachedTarget)
+        {
+            currentMousePosition = MouseInfo.getPointerInfo().getLocation();
 
-		Platform.runLater( new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				new Timeline( new KeyFrame( new Duration( moveTime ), new EventHandler<ActionEvent>()
-				{
-					@Override
-					public void handle( ActionEvent arg0 )
-					{
-						done.countDown();
-					}
-				}, new KeyValue( mouseXProperty, x, Interpolator.EASE_BOTH ), new KeyValue( mouseYProperty, y,
-						Interpolator.EASE_BOTH ) ) ).playFromStart();
-			}
-		} );
+            int directionX = Integer.compare( (int)x, (int)currentMousePosition.getX() );
+            int directionY = Integer.compare( (int)y, (int)currentMousePosition.getY() );
 
-		try
-		{
-			done.await();
-			FXTestUtils.awaitEvents();
-		}
-		catch( InterruptedException e )
-		{
-			throw new RuntimeException( e );
-		}
-	}
+            if(directionX == 0 && directionY == 0)
+                reachedTarget = true;
+            else
+            {
+                robot.mouseMove( (int)currentMousePosition.getX() + directionX, (int)currentMousePosition.getY() + directionY );
+            }
+            try
+            {
+                Thread.sleep( 1 );
+            }
+            catch( InterruptedException e )
+            {
+                e.printStackTrace();
+            }
+        }
 
-	@Override
-	public void press( MouseButton button )
-	{
-		if( button == null )
-		{
-			return;
-		}
-		robot.mousePress( BUTTONS.get( button ) );
-		FXTestUtils.awaitEvents();
-	}
+        FXTestUtils.awaitEvents();
+    }
 
-	@Override
-	public void release( MouseButton button )
-	{
-		if( button == null )
-		{
-			return;
-		}
-		robot.mouseRelease( BUTTONS.get( button ) );
-		FXTestUtils.awaitEvents();
-	}
+    @Override
+    public void press( MouseButton button )
+    {
+        if( button == null )
+        {
+            return;
+        }
+        robot.mousePress( BUTTONS.get( button ) );
+        FXTestUtils.awaitEvents();
+    }
 
-	@Override
-	public void press( KeyCode key )
-	{
-		robot.keyPress( key.impl_getCode() );
-		FXTestUtils.awaitEvents();
-	}
+    @Override
+    public void release( MouseButton button )
+    {
+        if( button == null )
+        {
+            return;
+        }
+        robot.mouseRelease( BUTTONS.get( button ) );
+        FXTestUtils.awaitEvents();
+    }
 
-	@Override
-	public void release( KeyCode key )
-	{
-		robot.keyRelease( key.impl_getCode() );
-		FXTestUtils.awaitEvents();
-	}
+    @Override
+    public void press( KeyCode key )
+    {
+        robot.keyPress( key.impl_getCode() );
+        FXTestUtils.awaitEvents();
+    }
 
-	@Override
-	public void scroll( int amount )
-	{
-		robot.mouseWheel( amount );
-		FXTestUtils.awaitEvents();
-	}
+    @Override
+    public void release( KeyCode key )
+    {
+        robot.keyRelease( key.impl_getCode() );
+        FXTestUtils.awaitEvents();
+    }
+
+    @Override
+    public void scroll( int amount )
+    {
+        robot.mouseWheel( amount );
+        FXTestUtils.awaitEvents();
+    }
 }
