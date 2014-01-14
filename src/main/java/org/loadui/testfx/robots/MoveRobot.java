f8c94a0f973d26ad972ecd2c7adc39cbf140fcd2
hastebrot <hastebrot@gmail.com>
2014-01-14 18:01:55 +0100

    Document methods in MoveRobot interface.

--- src/main/java/org/loadui/testfx/robots/MoveRobot.java
+++ src/main/java/org/loadui/testfx/robots/MoveRobot.java
@@ -9,17 +9,77 @@ import javafx.stage.Window;
 import org.hamcrest.Matcher;
 
 public interface MoveRobot {
+
+    /**
+     * Moves the mouse cursor to the given coordinates.
+     *
+     * @param x the X coordinate
+     * @param y the Y coordinate
+     */
     public MoveRobot moveTo(double x, double y);
 
+    /**
+     * Moves the mouse cursor to the given Point2D.
+     *
+     * @param point the Point2D
+     */
     public MoveRobot moveTo(Point2D point);
+
+    /**
+     * Moves the mouse cursor to the given Bounds.
+     *
+     * @param bounds the Bounds
+     */
     public MoveRobot moveTo(Bounds bounds);
+
+    /**
+     * Moves the mouse cursor to the given Node.
+     *
+     * @param node the Node
+     */
     public MoveRobot moveTo(Node node);
+
+    /**
+     * Moves the mouse cursor to the given Scene.
+     *
+     * @param scene the Scene
+     */
     public MoveRobot moveTo(Scene scene);
+
+    /**
+     * Moves the mouse cursor to the given Window.
+     *
+     * @param window the Window
+     */
     public MoveRobot moveTo(Window window);
 
+    /**
+     * Moves the mouse cursor to the Node of the given query.
+     *
+     * @param query the query
+     */
     public MoveRobot moveTo(String query);
+
+    /**
+     * Moves the mouse cursor to the Node of the given Matcher.
+     *
+     * @param matcher the Matcher
+     */
     public MoveRobot moveTo(Matcher<Object> matcher);
+
+    /**
+     * Moves the mouse cursor to the Node of the given Predicate.
+     *
+     * @param predicate the Predicate
+     */
     public MoveRobot moveTo(Predicate<Node> predicate);
 
+    /**
+     * Moves the mouse cursor relatively to its current position.
+     *
+     * @param x the X coordinate
+     * @param y the Y coordinate
+     */
     public MoveRobot moveBy(double x, double y);
+
 }
