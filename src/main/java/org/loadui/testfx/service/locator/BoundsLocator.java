8e49871d4a0a92233977c77be0c8a91fa1a754b5
hastebrot <hastebrot@gmail.com>
2014-01-14 01:17:12 +0100

    Create BoundsLocator and PointLocator.

---
+++ src/main/java/org/loadui/testfx/service/locator/BoundsLocator.java
@@ -0,0 +1,67 @@
+package org.loadui.testfx.service.locator;
+
+import javafx.geometry.BoundingBox;
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Window;
+
+public class BoundsLocator {
+
+    //---------------------------------------------------------------------------------------------
+    // METHODS.
+    //---------------------------------------------------------------------------------------------
+
+    public Bounds sceneBoundsFor(Node node) {
+        return node.localToScene(node.getBoundsInLocal());
+    }
+
+    public Bounds sceneBoundsVisibleFor(Node node) {
+        Bounds nodeBounds = sceneBoundsFor(node);
+        Bounds sceneBounds = sceneBoundsFor(node.getScene());
+        return intersectBounds(nodeBounds, sceneBounds);
+    }
+
+    public Bounds sceneBoundsFor(Scene scene) {
+        return new BoundingBox(0, 0, scene.getWidth(), scene.getHeight());
+    }
+
+    public Bounds screenBoundsFor(Bounds boundsInScene, Scene scene) {
+        Bounds sceneBoundsOnScreen = screenBoundsFor(scene);
+        double screenX = sceneBoundsOnScreen.getMinX() + boundsInScene.getMinX();
+        double screenY = sceneBoundsOnScreen.getMinY() + boundsInScene.getMinY();
+        return new BoundingBox(
+            screenX, screenY,
+            boundsInScene.getWidth(), boundsInScene.getHeight()
+        );
+    }
+
+    public Bounds screenBoundsFor(Scene scene) {
+        Window window = scene.getWindow();
+        double screenX = window.getX() + scene.getX();
+        double screenY = window.getY() + scene.getY();
+        return new BoundingBox(screenX, screenY, scene.getWidth(), scene.getHeight());
+    }
+
+    public Bounds screenBoundsFor(Window window) {
+        return new BoundingBox(
+            window.getX(), window.getY(),
+            window.getWidth(), window.getHeight()
+        );
+    }
+
+    //---------------------------------------------------------------------------------------------
+    // PRIVATE METHODS.
+    //---------------------------------------------------------------------------------------------
+
+    private Bounds intersectBounds(Bounds a, Bounds b) {
+        double minX = Math.max(a.getMinX(), b.getMinX());
+        double minY = Math.max(a.getMinY(), b.getMinY());
+        double maxX = Math.min(a.getMaxX(), b.getMaxX());
+        double maxY = Math.min(a.getMaxY(), b.getMaxY());
+        double width = maxX - minX;
+        double height = maxY - minY;
+        return new BoundingBox(minX, minY, width, height);
+    }
+
+}
---
+++ src/main/java/org/loadui/testfx/service/locator/PointLocator.java
@@ -0,0 +1,84 @@
+package org.loadui.testfx.service.locator;
+
+import javafx.geometry.Bounds;
+import javafx.geometry.HPos;
+import javafx.geometry.Point2D;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Window;
+
+public class PointLocator {
+
+    //---------------------------------------------------------------------------------------------
+    // FIELDS.
+    //---------------------------------------------------------------------------------------------
+
+    private BoundsLocator boundsLocator;
+
+    //---------------------------------------------------------------------------------------------
+    // CONSTRUCTORS.
+    //---------------------------------------------------------------------------------------------
+
+    public PointLocator(BoundsLocator boundsLocator) {
+        this.boundsLocator = boundsLocator;
+    }
+
+    //---------------------------------------------------------------------------------------------
+    // METHODS.
+    //---------------------------------------------------------------------------------------------
+
+    public Point2D pointFor(Bounds bounds, Pos position) {
+        return pointForBoundsPosition(bounds, position);
+    }
+
+    public Point2D pointFor(Node node, Pos position) {
+        Bounds nodeBounds = boundsLocator.sceneBoundsVisibleFor(node);
+        Bounds screenBounds = boundsLocator.screenBoundsFor(nodeBounds, node.getScene());
+        return pointFor(screenBounds, position);
+    }
+
+    public Point2D pointFor(Scene scene, Pos position) {
+        Bounds sceneBounds = boundsLocator.sceneBoundsFor(scene);
+        Bounds screenBounds = boundsLocator.screenBoundsFor(sceneBounds, scene);
+        return pointFor(screenBounds, position);
+    }
+
+    public Point2D pointFor(Window window, Pos position) {
+        Bounds screenBounds = boundsLocator.screenBoundsFor(window);
+        return this.pointFor(screenBounds, position);
+    }
+
+    //---------------------------------------------------------------------------------------------
+    // PRIVATE METHODS.
+    //---------------------------------------------------------------------------------------------
+
+    private Point2D pointForBoundsPosition(Bounds bounds, Pos position) {
+        double pointX = 0;
+        double pointY = 0;
+
+        if (position.getHpos() == HPos.LEFT) {
+            pointX = bounds.getMinX();
+        }
+        else if (position.getHpos() == HPos.CENTER) {
+            pointX = (bounds.getMinX() + bounds.getMaxX()) / 2;
+        }
+        else if (position.getHpos() == HPos.RIGHT) {
+            pointX = bounds.getMaxX();
+        }
+
+        if (position.getVpos() == VPos.TOP) {
+            pointY = bounds.getMinY();
+        }
+        else if (position.getVpos() == VPos.CENTER || position.getVpos() == VPos.BASELINE) {
+            pointY = (bounds.getMinY() + bounds.getMaxY()) / 2;
+        }
+        else if (position.getVpos() == VPos.BOTTOM) {
+            pointY = bounds.getMaxY();
+        }
+
+        return new Point2D(pointX, pointY);
+    }
+
+}
