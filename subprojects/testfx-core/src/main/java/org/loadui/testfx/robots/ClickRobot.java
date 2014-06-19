589f3b8225a521588ca8244812b4c402b61d1f52
hastebrot <hastebrot@gmail.com>
2014-06-19 14:33:48 +0200

    refactor(FxRobot): Add regression tests and create ClickRobotImpl.

--- subprojects/testfx-core/src/main/java/org/loadui/testfx/robots/ClickRobot.java
+++ subprojects/testfx-core/src/main/java/org/loadui/testfx/robots/ClickRobot.java
@@ -15,46 +15,17 @@
  */
 package org.loadui.testfx.robots;
 
-import com.google.common.base.Predicate;
-import javafx.geometry.Bounds;
-import javafx.geometry.Point2D;
-import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.input.MouseButton;
-import javafx.stage.Window;
-import org.hamcrest.Matcher;
+import org.loadui.testfx.service.query.PointQuery;
 
 public interface ClickRobot {
-    public ClickRobot click(MouseButton... buttons);
-    public ClickRobot click(double x, double y, MouseButton... buttons);
-    public ClickRobot click(Point2D point, MouseButton... buttons);
-    public ClickRobot click(Bounds bounds, MouseButton... buttons);
-    public ClickRobot click(Node node, MouseButton... buttons);
-    public ClickRobot click(Scene scene, MouseButton... buttons);
-    public ClickRobot click(Window window, MouseButton... buttons);
-    public ClickRobot click(String query, MouseButton... buttons);
-    public ClickRobot click(Matcher<Object> matcher, MouseButton... buttons);
-    public <T extends Node> ClickRobot click(Predicate<T> predicate, MouseButton... buttons);
 
-    public ClickRobot rightClick();
-    public ClickRobot rightClick(double x, double y);
-    public ClickRobot rightClick(Point2D point);
-    public ClickRobot rightClick(Bounds bounds);
-    public ClickRobot rightClick(Node node);
-    public ClickRobot rightClick(Scene scene);
-    public ClickRobot rightClick(Window window);
-    public ClickRobot rightClick(String query);
-    public ClickRobot rightClick(Matcher<Object> matcher);
-    public <T extends Node> ClickRobot rightClick(Predicate<T> predicate);
+    public void clickOn(MouseButton... buttons);
 
-    public ClickRobot doubleClick(MouseButton... buttons);
-    public ClickRobot doubleClick(double x, double y, MouseButton... buttons);
-    public ClickRobot doubleClick(Point2D point, MouseButton... buttons);
-    public ClickRobot doubleClick(Bounds bounds, MouseButton... buttons);
-    public ClickRobot doubleClick(Node node, MouseButton... buttons);
-    public ClickRobot doubleClick(Scene scene, MouseButton... buttons);
-    public ClickRobot doubleClick(Window window, MouseButton... buttons);
-    public ClickRobot doubleClick(String query, MouseButton... buttons);
-    public ClickRobot doubleClick(Matcher<Object> matcher, MouseButton... buttons);
-    public <T extends Node> ClickRobot doubleClick(Predicate<T> predicate, MouseButton... buttons);
+    public void clickOn(PointQuery pointQuery, MouseButton... buttons);
+
+    public void doubleClickOn(MouseButton... buttons);
+
+    public void doubleClickOn(PointQuery pointQuery, MouseButton... buttons);
+
 }
