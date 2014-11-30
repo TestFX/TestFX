256e474d5ea7212292d74086aa8ddf412028eb91
hastebrot <hastebrot@gmail.com>
2014-11-30 17:58:00 +0100

    refactor(API): Implement some matchers in ParentMatchers.

--- subprojects/testfx-core/src/main/java/org/testfx/matcher/base/ParentMatchers.java
+++ subprojects/testfx-core/src/main/java/org/testfx/matcher/base/ParentMatchers.java
@@ -1,9 +1,11 @@
 package org.testfx.matcher.base;
 
+import javafx.scene.Node;
 import javafx.scene.Parent;
 
 import org.hamcrest.Factory;
 import org.hamcrest.Matcher;
+import org.testfx.api.FxAssert;
 
 public class ParentMatchers {
 
@@ -13,12 +15,25 @@ public class ParentMatchers {
 
     @Factory
     public static Matcher<Parent> hasChild(String nodeQuery) {
+        Node node = FxAssert.assertContext().getNodeFinder().node(nodeQuery);
         return null;
     }
 
     @Factory
     public static Matcher<Parent> hasChildren(int amount, String nodeQuery) {
         return null;
+    }
+
+    //---------------------------------------------------------------------------------------------
+    // STATIC METHODS.
+    //---------------------------------------------------------------------------------------------
+
+    public static boolean hasChild(Parent parent) {
+        return !parent.getChildrenUnmodifiable().isEmpty();
+    }
+
+    public static boolean hasChildren(int amount, Parent parent) {
+        return parent.getChildrenUnmodifiable().size() == amount;
     }
 
 }
