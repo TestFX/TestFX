f9576b4a08bf177b81b07c10a79f353c711eba13
hastebrot <hastebrot@gmail.com>
2014-11-30 15:25:44 +0100

    feat(Assert): Implement typesafe assert.

---
+++ subprojects/testfx-core/src/main/java/org/testfx/api/FxAssert.java
@@ -0,0 +1,99 @@
+package org.testfx.api;
+
+import javafx.scene.Node;
+
+import com.google.common.base.Predicate;
+import org.hamcrest.Matcher;
+import org.hamcrest.MatcherAssert;
+import org.loadui.testfx.service.finder.NodeFinder;
+import org.loadui.testfx.service.finder.impl.NodeFinderImpl;
+import org.loadui.testfx.service.finder.impl.WindowFinderImpl;
+import org.testfx.matcher.base.PredicateMatchers;
+
+public class FxAssert {
+
+    //---------------------------------------------------------------------------------------------
+    // CONSTANTS.
+    //---------------------------------------------------------------------------------------------
+
+    private static final String EMPTY_STRING = "";
+
+    //---------------------------------------------------------------------------------------------
+    // STATIC FIELDS.
+    //---------------------------------------------------------------------------------------------
+
+    private static NodeFinder nodeFinder = new NodeFinderImpl(new WindowFinderImpl());
+
+    //---------------------------------------------------------------------------------------------
+    // STATIC METHODS.
+    //---------------------------------------------------------------------------------------------
+
+    public static <T extends Node> void verifyThat(String reason,
+                                                   T node,
+                                                   Matcher<T> nodeMatcher) {
+        verifyThatImpl(reason, node, nodeMatcher);
+    }
+
+    public static <T extends Node> void verifyThat(T node,
+                                                   Matcher<T> nodeMatcher) {
+        verifyThatImpl(emptyReason(), node, nodeMatcher);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Node> void verifyThat(String reason,
+                                                   String nodeQuery,
+                                                   Matcher<T> nodeMatcher) {
+        verifyThatImpl(reason, (T) toNode(nodeQuery), nodeMatcher);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Node> void verifyThat(String nodeQuery,
+                                                   Matcher<T> nodeMatcher) {
+        verifyThatImpl(emptyReason(), (T) toNode(nodeQuery), nodeMatcher);
+    }
+
+    public static <T extends Node> void verifyThat(String reason,
+                                                   T node,
+                                                   Predicate<T> nodePredicate) {
+        verifyThatImpl(reason, node, toNodeMatcher(nodePredicate));
+    }
+
+    public static <T extends Node> void verifyThat(T node,
+                                                   Predicate<T> nodePredicate) {
+        verifyThatImpl(emptyReason(), node, toNodeMatcher(nodePredicate));
+    }
+
+    //---------------------------------------------------------------------------------------------
+    // PRIVATE STATIC METHODS.
+    //---------------------------------------------------------------------------------------------
+
+    private static <T extends Node> void verifyThatImpl(String reason,
+                                                        T node,
+                                                        Matcher<T> nodeMatcher) {
+        try {
+            MatcherAssert.assertThat(reason, node, nodeMatcher);
+        }
+        catch (AssertionError error) {
+            // TODO: Save screenshot to file.
+            throw new AssertionError(error.getMessage());
+        }
+    }
+
+    private static String emptyReason() {
+        return EMPTY_STRING;
+    }
+
+    private static Node toNode(String nodeQuery) {
+        try {
+            return nodeFinder.node(nodeQuery);
+        }
+        catch (Exception ignore) {
+            return null;
+        }
+    }
+
+    private static <T extends Node> Matcher<T> toNodeMatcher(final Predicate<T> nodePredicate) {
+        return PredicateMatchers.nodeMatcher("applies on Predicate", nodePredicate);
+    }
+
+}
---
+++ subprojects/testfx-core/src/test/java/org/testfx/demo/FxAssertDemoTest.java
@@ -0,0 +1,105 @@
+package org.testfx.demo;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TreeView;
+import javafx.stage.Stage;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.loadui.testfx.framework.robot.FxRobot;
+import org.loadui.testfx.framework.robot.impl.FxRobotImpl;
+
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.api.FxLifecycle.registerPrimaryStage;
+import static org.testfx.api.FxLifecycle.setupApplication;
+import static org.testfx.matcher.base.NodeMatchers.isEnabled;
+import static org.testfx.matcher.base.NodeMatchers.isNotNull;
+import static org.testfx.matcher.base.NodeMatchers.isNull;
+import static org.testfx.matcher.base.NodeMatchers.isVisible;
+import static org.testfx.matcher.base.PredicateMatchers.nodeMatcher;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+
+public class FxAssertDemoTest {
+
+    //---------------------------------------------------------------------------------------------
+    // FIELDS.
+    //---------------------------------------------------------------------------------------------
+
+    FxRobot fx = new FxRobotImpl();
+
+    //---------------------------------------------------------------------------------------------
+    // FIXTURE METHODS.
+    //---------------------------------------------------------------------------------------------
+
+    @BeforeClass
+    public static void setupSpec() throws Exception {
+        registerPrimaryStage();
+    }
+
+    @Before
+    public void setup() throws Exception {
+        setupApplication(DemoApplication.class);
+    }
+
+    //---------------------------------------------------------------------------------------------
+    // FEATURE METHODS.
+    //---------------------------------------------------------------------------------------------
+
+    @Test
+    public void nonExistingNode_is_null() {
+        // expect:
+        verifyThat("#nonExistingNode", isNull());
+    }
+
+    @Test
+    public void button_is_not_null() {
+        // expect:
+        verifyThat("#button", isNotNull());
+    }
+
+    @Test
+    public void button_is_enabled() {
+        // expect:
+        verifyThat("#button", isEnabled());
+    }
+
+    @Test
+    public void button_is_visible() {
+        // expect:
+        verifyThat("#button", isVisible());
+    }
+
+    @Test
+    public void button_has_label() {
+        // when:
+        fx.clickOn("#button");
+
+        // then:
+        verifyThat("#button", hasText("clicked!"));
+    }
+
+    @Test
+    public void foo() {
+        verifyThat("#button", nodeMatcher(TreeView.class, "is true", (TreeView input) -> true));
+    }
+
+    //---------------------------------------------------------------------------------------------
+    // TEST FIXTURES.
+    //---------------------------------------------------------------------------------------------
+
+    public static class DemoApplication extends Application {
+        @Override
+        public void start(Stage stage) {
+            final Button button = new Button("click me!");
+            button.setId("button");
+            button.setOnAction(actionEvent -> button.setText("clicked!"));
+            Scene scene = new Scene(button, 600, 400);
+            stage.setScene(scene);
+            stage.setTitle(this.getClass().getSimpleName());
+        }
+    }
+
+}
