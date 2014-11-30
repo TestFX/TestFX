1711e7bfdf04cd6f072616907e54842b69921426
hastebrot <hastebrot@gmail.com>
2014-11-30 14:54:15 +0100

    feat(JUnit): Add guard method for headless environments from GuiTest.

--- subprojects/testfx-junit/src/main/java/org/testfx/framework/junit/FxRobotTestBase.java
+++ subprojects/testfx-junit/src/main/java/org/testfx/framework/junit/FxRobotTestBase.java
@@ -1,8 +1,12 @@
 package org.testfx.framework.junit;
 
+import java.awt.GraphicsEnvironment;
+
 import org.junit.BeforeClass;
 import org.loadui.testfx.framework.robot.impl.FxRobotImpl;
 import org.testfx.api.FxLifecycle;
+
+import static org.junit.Assume.assumeFalse;
 
 public abstract class FxRobotTestBase extends FxRobotImpl {
 
@@ -13,6 +17,10 @@ public abstract class FxRobotTestBase extends FxRobotImpl {
     @BeforeClass
     public static void internalSetupSpec()
                                   throws Exception {
+        assumeFalse(
+            "Cannot run JavaFX in headless environment",
+            GraphicsEnvironment.getLocalGraphicsEnvironment().isHeadlessInstance()
+        );
         FxLifecycle.registerPrimaryStage();
     }
 
