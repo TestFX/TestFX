1660c69f62a50d21bfd5bde55e97ea70d9552bb0
Jan Ortner <jan@192.168.0.126>
2016-05-16 19:11:28 +0200

    271: Disable TestFX for CI
    Task-Url: https://github.com/TestFX/TestFX/issues/271
    
    Fixed an issue with missing License header.
    Added some more comments.

--- subprojects/testfx-junit/src/test/java/org/testfx/framework/junit/ApplicationTestTest.java
+++ subprojects/testfx-junit/src/test/java/org/testfx/framework/junit/ApplicationTestTest.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2013-2014 SmartBear Software
+ * Copyright 2014-2015 The TestFX Contributors
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the
+ * European Commission - subsequent versions of the EUPL (the "Licence"); You may
+ * not use this work except in compliance with the Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ * http://ec.europa.eu/idabc/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the Licence for the
+ * specific language governing permissions and limitations under the Licence.
+ */
+
 package org.testfx.framework.junit;
 
 import static org.junit.Assert.assertTrue;
@@ -9,8 +26,15 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+/**
+ * Class for testing features of {@link ApplicationTest}.
+ * 
+ */
 public class ApplicationTestTest {
 
+	/**
+	 * Tests that the application runs with doUITest set to true.
+	 */
 	@Test public void shouldRunTest(){
 		DutClass dut=new DutClass();
 		assertTrue(!dut.started);
@@ -24,6 +48,9 @@ public class ApplicationTestTest {
 		}
 	}
 
+	/**
+	 * Tests that the application does not run with doUITest set to false.
+	 */
 	@Test public void shouldNotRunTest(){
 		DutClass dut=new DutClass();
 		assertTrue(!dut.started);
