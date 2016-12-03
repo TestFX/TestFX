d9acaae52f53e775b093c18061fc1965bb99a185
Jordan Martinez <jordanalex.martinez@gmail.com>
2016-12-02 22:09:17 -0800

    (doc) Add javadoc to ScrollRobot

--- subprojects/testfx-core/src/main/java/org/testfx/robot/ScrollRobot.java
+++ subprojects/testfx-core/src/main/java/org/testfx/robot/ScrollRobot.java
@@ -21,19 +21,56 @@ import javafx.geometry.VerticalDirection;
 
 public interface ScrollRobot {
 
+    /**
+     * Scrolls vertically by amount (in terms of ticks of a mouse wheel): if {@code amount >= 0}, up; otherwise, down.
+     *
+     * @param amount of a mouse wheel's scroll ticks
+     */
     void scroll(int amount);
 
+    /**
+     * Scrolls vertically by amount (in terms of ticks of a mouse wheel) in given direction.
+     *
+     * @param positiveAmount the number of scroll ticks
+     * @param direction whether to scroll up or down
+     */
     void scroll(int positiveAmount,
                 VerticalDirection direction);
 
+    /**
+     * Scrolls up by amount (in terms of ticks of a mouse wheel).
+     * .
+     * @param positiveAmount the number of scroll ticks
+     */
     void scrollUp(int positiveAmount);
 
+    /**
+     * Scrolls down by amount (in terms of ticks of a wheel).
+     *
+     * @param positiveAmount the number of scroll ticks
+     */
     void scrollDown(int positiveAmount);
 
+    /**
+     * Scrolls horizontally by amount (in terms of ticks of a mouse wheel) in given direction.
+     *
+     * @param positiveAmount the number of scroll ticks
+     * @param direction whether to scroll left or right
+     */
     void scroll(int positiveAmount,
                 HorizontalDirection direction);
 
+    /**
+     * Scrolls left by amount (in terms of ticks of a wheel).
+     *
+     * @param positiveAmount the number of scroll ticks
+     */
     void scrollLeft(int positiveAmount);
 
+    /**
+     * Scrolls right by amount (in terms of ticks of a wheel).
+     *
+     * @param positiveAmount the number of scroll ticks
+     */
     void scrollRight(int positiveAmount);
 }
