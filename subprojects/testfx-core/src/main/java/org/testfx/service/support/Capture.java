bb8e62a612887c6983ed337357bf325d43ddd1a3
Jordan Martinez <jordanalex.martinez@gmail.com>
2016-12-07 23:08:39 -0800

    (doc) Add documentation to Capture

--- subprojects/testfx-core/src/main/java/org/testfx/service/support/Capture.java
+++ subprojects/testfx-core/src/main/java/org/testfx/service/support/Capture.java
@@ -18,6 +18,10 @@ package org.testfx.service.support;
 
 import javafx.scene.image.Image;
 
+/**
+ * Essentially a {@link java.util.function.Supplier} that returns an {@link Image} via {@link #getImage()}
+ */
+@FunctionalInterface
 public interface Capture {
 
     Image getImage();
