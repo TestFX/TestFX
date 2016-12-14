196eb0e02f9f0991e43a8c641b2f082c4e3597ed
Alex Simkin <asimkin@atlassian.com>
2016-12-14 11:51:32 +1100

    Junit5 adapter

---
+++ subprojects/testfx-junit5/src/main/java/org/testfx/framework/junit5/ApplicationExtension.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2013-2014 SmartBear Software
+ * Copyright 2014-2015 The TestFX Contributors
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the
+ * European Commission - subsequent versions of the EUPL (the "Licence"); You may
+ * not use this work except in compliance with the Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ * http://ec.europa.eu/idabc/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the Licence for the
+ * specific language governing permissions and limitations under the Licence.
+ */
+package org.testfx.framework.junit5;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.stage.Stage;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.jupiter.api.extension.TestExtensionContext;
+import org.junit.jupiter.api.extension.TestInstancePostProcessor;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
+
+public class ApplicationExtension extends FxRobot implements BeforeEachCallback, AfterEachCallback,
+        TestInstancePostProcessor, ParameterResolver {
+
+    private ApplicationFixture applicationFixture;
+
+    @Override
+    public void postProcessTestInstance(Object testInstance, ExtensionContext context) throws Exception {
+        if (testInstance instanceof ApplicationFixture) {
+            applicationFixture = (ApplicationFixture) testInstance;
+        } else {
+            List<Method> init = new ArrayList<>();
+            List<Method> start = new ArrayList<>();
+            List<Method> stop = new ArrayList<>();
+            Method[] methods = testInstance.getClass().getDeclaredMethods();
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(Init.class)) {
+                    init.add(method);
+                }
+                if (method.isAnnotationPresent(Start.class)) {
+                    start.add(method);
+                }
+                if (method.isAnnotationPresent(Stop.class)) {
+                    stop.add(method);
+                }
+            }
+            applicationFixture = new AnnotationBasedApplicationFixture(testInstance, init, start, stop);
+        }
+    }
+
+    @Override
+    public boolean supports(ParameterContext parameterContext, ExtensionContext extensionContext) {
+        return parameterContext.getParameter().getType().isAssignableFrom(FxRobot.class);
+    }
+
+    @Override
+    public Object resolve(ParameterContext parameterContext, ExtensionContext extensionContext) {
+        return this;
+    }
+
+    @Override
+    public void beforeEach(TestExtensionContext context) throws Exception {
+        FxToolkit.registerPrimaryStage();
+        FxToolkit.setupApplication(() -> new ApplicationAdapter(applicationFixture));
+    }
+
+    @Override
+    public void afterEach(TestExtensionContext context) throws Exception {
+        FxToolkit.cleanupApplication(new ApplicationAdapter(applicationFixture));
+    }
+
+    private static class AnnotationBasedApplicationFixture implements ApplicationFixture {
+
+        private final Object testInstance;
+        private final List<Method> init;
+        private final List<Method> start;
+        private final List<Method> stop;
+
+        private AnnotationBasedApplicationFixture(Object testInstance, List<Method> init,
+                                                  List<Method> start, List<Method> stop) {
+            this.testInstance = testInstance;
+            this.init = init;
+            this.start = start;
+            this.stop = stop;
+        }
+
+        @Override
+        public void init() throws InvocationTargetException, IllegalAccessException {
+            for (Method method : init) {
+                method.invoke(testInstance);
+            }
+        }
+
+        @Override
+        public void start(Stage stage) throws InvocationTargetException, IllegalAccessException {
+            for (Method method : start) {
+                method.invoke(testInstance, stage);
+            }
+        }
+
+        @Override
+        public void stop() throws InvocationTargetException, IllegalAccessException {
+            for (Method method : stop) {
+                method.invoke(testInstance);
+            }
+        }
+
+    }
+
+}
---
+++ subprojects/testfx-junit5/src/test/java/org/testfx/framework/junit/KeyAndButtonReleaseTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2013-2014 SmartBear Software
+ * Copyright 2014-2015 The TestFX Contributors
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the
+ * European Commission - subsequent versions of the EUPL (the "Licence"); You may
+ * not use this work except in compliance with the Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ * http://ec.europa.eu/idabc/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the Licence for the
+ * specific language governing permissions and limitations under the Licence.
+ */
+package org.testfx.framework.junit;
+
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
+
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Tests to see if pressed keys/buttons are released after a test is run.
+ *
+ * For keys, this prevents hanging issues (if Shortcut key is pressed) or deletion issues (if backspace/delete key
+ * is pressed) or insertion issues (if anything else is pressed). The same for mouse buttons
+ */
+
+class KeyAndButtonReleaseTest extends ApplicationTest {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+
+    @Test
+    void b_When_a_test_forgets_to_release_keys() {
+        press(KeyCode.CONTROL, KeyCode.SHIFT, KeyCode.ALT);
+    }
+
+    @Test
+    void c_Then_keys_are_not_pressed() {
+        assertThat(robotContext().getKeyboardRobot().getPressedKeys().isEmpty(), is(true));
+    }
+
+    @Test
+    void e_When_a_test_forgets_to_release_buttons() {
+        press(MouseButton.PRIMARY, MouseButton.SECONDARY, MouseButton.MIDDLE);
+    }
+
+    @Test
+    void f_Then_buttons_are_not_pressed() {
+        assertThat(robotContext().getMouseRobot().getPressedButtons().isEmpty(), is(true));
+    }
+}
