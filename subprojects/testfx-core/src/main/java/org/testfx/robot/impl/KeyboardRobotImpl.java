c66256bb66298426181c7eade1f853b1152714d2
Michael Ennen <mike.ennen@gmail.com>
2017-01-29 23:27:59 -0700

    (refactor) Replace Guava newX() calls with regular constructors.

--- subprojects/testfx-core/src/main/java/org/testfx/robot/impl/KeyboardRobotImpl.java
+++ subprojects/testfx-core/src/main/java/org/testfx/robot/impl/KeyboardRobotImpl.java
@@ -17,13 +17,13 @@
 package org.testfx.robot.impl;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 import javafx.scene.input.KeyCode;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import org.testfx.api.annotation.Unstable;
 import org.testfx.robot.BaseRobot;
 import org.testfx.robot.KeyboardRobot;
@@ -52,7 +52,7 @@ public class KeyboardRobotImpl implements KeyboardRobot {
     // PRIVATE FIELDS.
     //---------------------------------------------------------------------------------------------
 
-    private final Set<KeyCode> pressedKeys = Sets.newHashSet();
+    private final Set<KeyCode> pressedKeys = new HashSet<>();
     public final Set<KeyCode> getPressedKeys() {
         return Collections.unmodifiableSet(pressedKeys);
     }
