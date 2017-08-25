8539d7506c87b4f2ddd0ae7c242bcb1cdd7eee18
Jordan Martinez <jordanalex.martinez@gmail.com>
2017-08-25 13:12:58 -0700

    (refactor) Output saved image's path in error's message

--- subprojects/testfx-core/src/main/java/org/testfx/util/DebugUtils.java
+++ subprojects/testfx-core/src/main/java/org/testfx/util/DebugUtils.java
@@ -56,8 +56,10 @@ import org.testfx.service.support.FiredEvents;
  *     When a test fails, an image of the test can be captured and saved to a PNG file. Supports a number
  *     of image sizes: a screenshot, a window, an area of the screen (i.e. bounds), or an individual node.
  *     Use the convenience methods prefixed by "save" (e.g. {@link #saveScreenshot(String)}) to quickly capture
- *     and save images, or create your own via {@link #saveTestImage(Function, Supplier)},
- *     {@link #captureBounds(Bounds)}, and your own {@code Supplier<Path>}.
+ *     and save images. The image path will be outputted using {@link #insertContent(String, Object)} and
+ *     will appear in the error's message.
+ *     For your own custom-built handler, use {@link #saveTestImage(Function, Supplier, String)}, one of the
+ *     "capture"-prefixed methods, and your own {@code Supplier<Path>}.
  * </p>
  * <p>
  *     This class follows the given philosophy in how to add additional information to the {@link AssertionError}'s
@@ -353,14 +355,15 @@ public final class DebugUtils {
      * (e.g. "button_has_label - 2.png").
      */
     public static Function<StringBuilder, StringBuilder> saveScreenshot(String testName, int photoNumber) {
-        return saveScreenshot(defaultImagePath(testName, photoNumber));
+        return saveScreenshot(defaultImagePath(testName, photoNumber), DEFAULT_INDENT);
     }
 
     /**
      * Saves the captured screenshot of the primary screen to the supplied path.
      */
-    public static Function<StringBuilder, StringBuilder> saveScreenshot(Supplier<Path> capturedImagePath) {
-        return saveTestImage(captureScreenshot(), capturedImagePath);
+    public static Function<StringBuilder, StringBuilder> saveScreenshot(Supplier<Path> capturedImagePath,
+                                                                        String indent) {
+        return saveTestImage(captureScreenshot(), capturedImagePath, indent);
     }
 
     /**
@@ -368,15 +371,16 @@ public final class DebugUtils {
      */
     public static Function<StringBuilder, StringBuilder> saveScreenshot(Screen screen,
                                                                         String testName, int photoNumber) {
-        return saveScreenshot(screen, defaultImagePath(testName, photoNumber));
+        return saveScreenshot(screen, defaultImagePath(testName, photoNumber), DEFAULT_INDENT);
     }
 
     /**
      * Saves the captured screenshot of the screen to the supplied path.
      */
     public static Function<StringBuilder, StringBuilder> saveScreenshot(Screen screen,
-                                                                        Supplier<Path> capturedImagePath) {
-        return saveTestImage(captureScreenshot(screen), capturedImagePath);
+                                                                        Supplier<Path> capturedImagePath,
+                                                                        String indent) {
+        return saveTestImage(captureScreenshot(screen), capturedImagePath, indent);
     }
 
     /**
@@ -385,7 +389,7 @@ public final class DebugUtils {
      */
     public static Function<StringBuilder, StringBuilder> saveScreenshot(int screenIndex,
                                                                         String testName, int photoNumber) {
-        return saveScreenshot(screenIndex, defaultImagePath(testName, photoNumber));
+        return saveScreenshot(screenIndex, defaultImagePath(testName, photoNumber), DEFAULT_INDENT);
     }
 
     /**
@@ -393,22 +397,22 @@ public final class DebugUtils {
      * to the supplied path.
      */
     public static Function<StringBuilder, StringBuilder> saveScreenshot(int screenIndex,
-                                                                        Supplier<Path> capturedImagePath) {
-        return saveTestImage(captureScreenshot(screenIndex), capturedImagePath);
+                                                                        Supplier<Path> capturedImagePath, String indent) {
+        return saveTestImage(captureScreenshot(screenIndex), capturedImagePath, indent);
     }
 
     /**
      * Saves the captured registered stage "testName - photoNumber.png" (e.g. "button_has_label - 2.png").
      */
     public static Function<StringBuilder, StringBuilder> saveWindow(String testName, int photoNumber) {
-        return saveWindow(defaultImagePath(testName, photoNumber));
+        return saveWindow(defaultImagePath(testName, photoNumber), DEFAULT_INDENT);
     }
 
     /**
      * Saves the captured registered stage to the supplied path
      */
-    public static Function<StringBuilder, StringBuilder> saveWindow(Supplier<Path> capturedImagePath) {
-        return saveWindow(FxToolkit.toolkitContext().getRegisteredStage(), capturedImagePath);
+    public static Function<StringBuilder, StringBuilder> saveWindow(Supplier<Path> capturedImagePath, String indent) {
+        return saveWindow(FxToolkit.toolkitContext().getRegisteredStage(), capturedImagePath, indent);
     }
 
     /**
@@ -416,15 +420,15 @@ public final class DebugUtils {
      */
     public static Function<StringBuilder, StringBuilder> saveWindow(Window window,
                                                                     String testName, int photoNumber) {
-        return saveWindow(window, defaultImagePath(testName, photoNumber));
+        return saveWindow(window, defaultImagePath(testName, photoNumber), DEFAULT_INDENT);
     }
 
     /**
      * Saves the captured window to the supplied path
      */
-    public static Function<StringBuilder, StringBuilder> saveWindow(Window window,
-                                                                        Supplier<Path> capturedImagePath) {
-        return saveTestImage(captureWindow(window), capturedImagePath);
+    public static Function<StringBuilder, StringBuilder> saveWindow(Window window, Supplier<Path> capturedImagePath,
+                                                                    String indent) {
+        return saveTestImage(captureWindow(window), capturedImagePath, indent);
     }
 
 
@@ -433,14 +437,15 @@ public final class DebugUtils {
      * (e.g. "button_has_label - 2.png").
      */
     public static Function<StringBuilder, StringBuilder> saveBounds(Bounds bounds, String testName, int photoNumber) {
-        return saveBounds(mapToRect2D(bounds), testName, photoNumber);
+        return saveBounds(bounds, defaultImagePath(testName, photoNumber), DEFAULT_INDENT);
     }
 
     /**
      * Saves the captured image based on the given bounds to the supplied path.
      */
-    public static Function<StringBuilder, StringBuilder> saveBounds(Bounds bounds, Supplier<Path> capturedImagePath) {
-        return saveTestImage(captureBounds(bounds), capturedImagePath);
+    public static Function<StringBuilder, StringBuilder> saveBounds(Bounds bounds, Supplier<Path> capturedImagePath,
+                                                                    String indent) {
+        return saveTestImage(captureBounds(bounds), capturedImagePath, indent);
     }
 
     /**
@@ -449,42 +454,48 @@ public final class DebugUtils {
      */
     public static Function<StringBuilder, StringBuilder> saveBounds(Rectangle2D bounds,
                                                                     String testName, int photoNumber) {
-        return saveBounds(bounds, defaultImagePath(testName, photoNumber));
+        return saveBounds(bounds, defaultImagePath(testName, photoNumber), DEFAULT_INDENT);
     }
 
     /**
      * Saves the captured image based on the given bounds to the supplied path.
      */
     public static Function<StringBuilder, StringBuilder> saveBounds(Rectangle2D bounds,
-                                                                    Supplier<Path> capturedImagePath) {
-        return saveTestImage(captureBounds(bounds), capturedImagePath);
+                                                                    Supplier<Path> capturedImagePath, String indent) {
+        return saveTestImage(captureBounds(bounds), capturedImagePath, indent);
     }
 
     /**
      * Saves the captured node image to "testName - photoNumber.png" (e.g. "button_has_label - 2.png").
      */
     public static Function<StringBuilder, StringBuilder> saveNode(Node node, String testName, int photoNumber) {
-        return saveNode(node, defaultImagePath(testName, photoNumber));
+        return saveNode(node, defaultImagePath(testName, photoNumber), DEFAULT_INDENT);
     }
 
     /**
      * Saves the captured node to the supplied path.
      */
-    public static Function<StringBuilder, StringBuilder> saveNode(Node node, Supplier<Path> capturedImagePath) {
-        return saveTestImage(captureNode(node), capturedImagePath);
+    public static Function<StringBuilder, StringBuilder> saveNode(Node node,
+                                                                  Supplier<Path> capturedImagePath,
+                                                                  String indent) {
+        return saveTestImage(captureNode(node), capturedImagePath, indent);
     }
 
     /**
      * Saves the captured image to the supplied path.
      */
     public static Function<StringBuilder, StringBuilder> saveTestImage(Function<CaptureSupport, Image> captureImage,
-                                                                       Supplier<Path> capturedImagePath) {
-        return runCode(() -> {
+                                                                       Supplier<Path> capturedImagePath,
+                                                                       String indent) {
+        return sb -> {
             CaptureSupport captureSupport = FxService.serviceContext().getCaptureSupport();
             Image errorImage = captureImage.apply(captureSupport);
 
-            captureSupport.saveImage(errorImage, capturedImagePath.get());
-        });
+            Path path = capturedImagePath.get();
+            captureSupport.saveImage(errorImage, path);
+            insertContent("Test image saved at:", path.toAbsolutePath(), indent).apply(sb);
+            return sb;
+        };
     }
 
     //---------------------------------------------------------------------------------------------
