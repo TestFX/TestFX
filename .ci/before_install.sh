cfa05043faee0e7e388d30a4be9962f646c1eef3
Michael Ennen <mike.ennen@gmail.com>
2017-08-29 21:57:45 -0700

    (chore) Remove Travis macOS "partial headless" build.

--- .ci/before_install.sh
+++ .ci/before_install.sh
@@ -3,9 +3,8 @@
 # vim :set ts=2 sw=2 sts=2 et:
 if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
   brew update
-  brew upgrade
-  brew cask install java
-  brew install gradle
+  brew cask reinstall java
+  brew outdated gradle || brew upgrade gradle
   brew unlink python # fixes 'run_one_line' is not defined error in backtrace
 fi
 
