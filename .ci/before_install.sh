48ec21f7ff5c69b6b6a8af03a4ce1dd2820016dc
Michael Ennen <mike.ennen@gmail.com>
2017-10-08 23:30:24 -0700

    (chore) Add Java 9 build for macOS to Travis.

--- .ci/before_install.sh
+++ .ci/before_install.sh
@@ -3,7 +3,16 @@
 # vim :set ts=2 sw=2 sts=2 et:
 if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
   brew update
-  brew cask reinstall java
+  brew install jenv
+  jenv versions
+  if [[ "${TRAVIS_JAVA_VERSION}" == 8 ]]; then
+    brew cask reinstall java8
+  elif [[ "${TRAVIS_JAVA_VERSION}" == 9 ]]; then
+    brew cask reinstall java
+  else
+      echo "TRAVIS_JAVA_VERSION environment variable not set!"
+  fi
+
   brew outdated gradle || brew upgrade gradle
   brew unlink python # fixes 'run_one_line' is not defined error in backtrace
 fi
