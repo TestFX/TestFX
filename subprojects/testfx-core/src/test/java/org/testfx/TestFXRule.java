d0921243376a36a591b09f62f23d8e3c3f932106
Michael Ennen <mike.ennen@gmail.com>
2017-11-15 14:33:09 -0700

    (fix) Attempt known flaky tests multiple times.

--- subprojects/testfx-core/src/test/java/org/testfx/TestFXRule.java
+++ subprojects/testfx-core/src/test/java/org/testfx/TestFXRule.java
@@ -20,13 +20,50 @@ import java.util.concurrent.Callable;
 
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 import org.testfx.api.FxToolkit;
 import org.testfx.util.WaitForAsyncUtils;
 
 public class TestFXRule extends TestWatcher {
 
+    private final int retryCount;
     private static final long WAIT_MILLIS = 30000;
     public static boolean initialized;
+
+    public TestFXRule() {
+        this.retryCount = 0;
+    }
+
+    public TestFXRule(int retryCount) {
+        this.retryCount = retryCount;
+    }
+
+    public Statement apply(Statement base, Description description) {
+        return statement(base, description);
+    }
+
+    private Statement statement(final Statement base, final Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                Throwable caughtThrowable = null;
+
+                for (int i = 0; i < retryCount; i++) {
+                    try {
+                        base.evaluate();
+                        return;
+                    } catch (Throwable t) {
+                        caughtThrowable = t;
+                        System.err.println(description.getDisplayName() + ": run " + (i + 1) + " failed");
+                    }
+                }
+                System.err.println(description.getDisplayName() + ": giving up after " + retryCount + " failures");
+                if (caughtThrowable != null) {
+                    throw caughtThrowable;
+                }
+            }
+        };
+    }
 
     @Override
     protected void starting(Description description) {
