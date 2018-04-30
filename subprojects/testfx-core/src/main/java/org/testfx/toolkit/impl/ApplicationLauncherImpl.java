e799699188f2e1dca80f58a03527e766e865892c
Michael Ennen <mike.ennen@gmail.com>
2018-04-30 01:48:47 -0700

    d

--- subprojects/testfx-core/src/main/java/org/testfx/toolkit/impl/ApplicationLauncherImpl.java
+++ subprojects/testfx-core/src/main/java/org/testfx/toolkit/impl/ApplicationLauncherImpl.java
@@ -18,7 +18,6 @@ package org.testfx.toolkit.impl;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.util.Objects;
 import javafx.application.Application;
 
 import org.testfx.toolkit.ApplicationLauncher;
