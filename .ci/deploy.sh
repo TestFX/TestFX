f8ca087dd1245e8b9fd4fcf5e3dbaf55dd24d74e
Michael Ennen <mike.ennen@gmail.com>
2018-10-09 12:55:08 -0700

    (chore) Add output when deploy syncs to Maven Central.

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -25,11 +25,12 @@ version=$(git tag -l --points-at HEAD) # Replace with $TRAVIS_TAG?
 declare -a subprojects=("testfx-core" "testfx-junit" "testfx-junit5" "testfx-spock")
 for subproject in "${subprojects[@]}"
 do
-  curl -u "$BINTRAY_USERNAME":"$BINTRAY_API_KEY" \
+  curl -i \
+       -u "$BINTRAY_USERNAME":"$BINTRAY_API_KEY" \
        -H "Content-Type: application/json" \
        -X POST \
        -d "{\"username\":\"$SONATYPE_USERNAME\",\"password\":\"$SONATYPE_PASSWORD\",\"close\":\"1\"}" \
-       https://bintray.com/api/v1/central_sync/testfx/testfx/"$subproject"/versions/"${version:1}"
+       https://api.bintray.com/maven_central_sync/testfx/testfx/"$subproject"/versions/"${version:1}"
 done
 
 # vim :set ts=2 sw=2 sts=2 et:
