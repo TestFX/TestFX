e48a0af61a807021a1e9f04a8393979e6374d6a3
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2019-07-31 19:16:31 -0400

    documentation fixes

--- subprojects/testfx-core/src/main/java/org/testfx/robot/Motion.java
+++ subprojects/testfx-core/src/main/java/org/testfx/robot/Motion.java
@@ -38,7 +38,7 @@ package org.testfx.robot;
  * <p>
  * Traveling in a straight-line between a and b (that is, tracing the hypotenuse) is
  * {@code DIRECT}. Traveling first from a to c and then from c to b is {@code HORIZONTAL_FIRST}.
- * Traveling first from a to d and then from d to b is {@code VERTICAL_FIRST}. {@code DIRECT}
+ * Traveling first from a to d and then from d to b is {@code VERTICAL_FIRST}. {@code DEFAULT}
  * means that no specific type of motion was explicitly requested.
  */
 public enum Motion {
