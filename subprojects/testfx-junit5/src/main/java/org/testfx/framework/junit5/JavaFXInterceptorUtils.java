e88d05575ffb029331f9cb06a40f2632db084d85
rstein <r.steinhagen@gsi.de>
2020-05-09 10:02:04 +0200

    added JUnit5 extension to execute @Test within JavaFX UI thread
    Simple JUnit 5 extension to ensure that @test statements are executed in
    the JavaFX UI thread. This is (strictly) necessary when testing setter
    and/or getter methods of JavaFX classes (ie. Node derived, properties
    etc).
    
    See also issue #700
    https://github.com/TestFX/TestFX/issues/700
    
    and real-world usage in
    https://github.com/GSI-CS-CO/chart-fx/tree/master/chartfx-chart/src/test/java/de/gsi/chart

---
+++ subprojects/testfx-junit5/src/main/java/org/testfx/framework/junit5/JavaFXInterceptorUtils.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright 2013-2014 SmartBear Software
+ * Copyright 2014-2020 The TestFX Contributors
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the
+ * European Commission - subsequent versions of the EUPL (the "Licence"); You may
+ * not use this work except in compliance with the Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ * http://ec.europa.eu/idabc/eupl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the Licence for the
+ * specific language governing permissions and limitations under the Licence.
+ */
+package org.testfx.framework.junit5;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Method;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.InvocationInterceptor;
+import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
+import org.testfx.framework.junit5.utils.FXUtils;
+
+/**
+ * Simple JUnit 5 extension to ensure that {@code @Test} statements are executed in the JavaFX UI thread.
+ * This is (strictly) necessary when testing setter and/or getter methods of JavaFX classes (ie. Node derived, properties etc).
+ * <p>
+ * Use the
+ * <ul>
+ * <li> {@code @ExtendWith(avaFxInterceptor.class) } if all @Test, or
+ * <li> {@code @ExtendWith(SelectiveJavaFxInterceptor.class) } if only @Test + @TestFx annotated tests
+ * </ul>
+ * should be executed within the JavaFX UI thread.
+ *
+ * @author RalphSteinhagen
+ */
+public class JavaFXInterceptorUtils {
+    /**
+	 * Simple JUnit 5 extension to ensure that {@code @Test} statements are executed in the JavaFX UI thread.
+	 * This is (strictly) necessary when testing setter and/or getter methods of JavaFX classes (ie. Node derived, properties etc).
+	 * <p>
+	 * Example usage:
+	 * <pre><code>
+	 * @ExtendWith(ApplicationExtension.class)
+	 * @ExtendWith(JavaFxInterceptor.class)
+	 * public class SquareButtonTest {
+	 *     @Start
+	 *     public void start(Stage stage) {
+	 *         // usual FX initialisation
+	 *         // ...
+	 *     }
+	 *
+	 *    @TestFx // note: this is equivalent to {@code @Test} when using {@code @ExtendWith(JavaFxInterceptor.class)}
+	 *    public void testJavaFxThreadSafety() {
+	 *        // verifies that this test is indeed executed in the JavaFX thread
+	 *        assertTrue(Platform.isFxApplicationThread());
+	 *
+	 *        // perform the regular JavaFX thread safe assertion tests
+	 *        // ...
+	 *    }
+	 *
+	 *    @Test // also executed in JavaFX thread, for different behaviour use:  {@code @ExtendWith(SelectiveJavaFxInterceptor.class)}
+	 *    public void testNonJavaFx() {
+	 *        // verifies that this test is also executed in the JavaFX thread
+	 *        assertTrue(Platform.isFxApplicationThread());
+	 *
+	 *        // perform regular assertion tests within the JavaFX thread
+	 *        // ...
+	 *    }
+	 * }
+	 *
+	 * </code></pre>
+	 *
+	 * @author rstein
+	 */
+    public static class JavaFxInterceptor implements InvocationInterceptor {
+        @Override
+        public void interceptTestMethod(final Invocation<Void> invocation,
+                final ReflectiveInvocationContext<Method> invocationContext,
+                final ExtensionContext extensionContext) throws Throwable {
+            final AtomicReference<Throwable> throwable = new AtomicReference<>();
+
+            // N.B. explicit run and wait since the test should only continue
+            // if the previous JavaFX access as been finished.
+            FXUtils.runAndWait(() -> {
+                try {
+                    // executes function after @Test
+                    invocation.proceed();
+                } catch (final Throwable t) {
+                    throwable.set(t);
+                }
+            });
+            final Throwable t = throwable.get();
+            if (t != null) {
+                throw t;
+            }
+        }
+    }
+
+    /**
+	 * Simple JUnit 5 extension to ensure that {@code @Test} statements are executed in the JavaFX UI thread.
+	 * This is (strictly) necessary when testing setter and/or getter methods of JavaFX classes (ie. Node derived, properties etc).
+	 * <p>
+	 * Example usage:
+	 * <pre><code>
+	 * @ExtendWith(ApplicationExtension.class)
+	 * @ExtendWith(SelectiveJavaFxInterceptor.class)
+	 * public class SquareButtonTest {
+	 *     @Start
+	 *     public void start(Stage stage) {
+	 *         // usual FX initialisation
+	 *         // ...
+	 *     }
+	 *
+	 *    @TestFx // forces execution in JavaFX thread
+	 *    public void testJavaFxThreadSafety() {
+	 *        // verifies that this test is indeed executed in the JavaFX thread
+	 *        assertTrue(Platform.isFxApplicationThread());
+	 *
+	 *        // perform the regular JavaFX thread safe assertion tests
+	 *        // ...
+	 *    }
+	 *
+	 *    @Test // explicitly not executed in JavaFX thread; for different behaviour use:  {@code @ExtendWith(JavaFxInterceptor.class)}
+	 *    public void testNonJavaFx() {
+	 *        // verifies that this test is not executed within the JavaFX thread
+	 *        assertFalse(Platform.isFxApplicationThread());
+	 *
+	 *        // perform the regular non-JavaFX thread-related assertion tests
+	 *        // ...
+	 *    }
+	 * }
+	 *
+	 * </code></pre>
+	 *
+	 * @author rstein
+	 */
+    public static class SelectiveJavaFxInterceptor implements InvocationInterceptor {
+        @Override
+        public void interceptTestMethod(final Invocation<Void> invocation,
+                final ReflectiveInvocationContext<Method> invocationContext,
+                final ExtensionContext extensionContext) throws Throwable {
+            final AtomicReference<Throwable> throwable = new AtomicReference<>();
+
+            boolean isFxAnnotation = false;
+            final Optional<AnnotatedElement> element = extensionContext.getElement();
+            if (element.isPresent()) {
+                for (final Annotation annotation : element.get().getAnnotations()) {
+                    if (annotation.annotationType().equals(TestFx.class)) {
+                        isFxAnnotation = true;
+                    }
+                }
+            }
+
+            final Runnable testToBeExecuted = () -> {
+                try {
+                    // executes function after @Test
+                    invocation.proceed();
+                } catch (final Throwable t) {
+                    throwable.set(t);
+                }
+            };
+
+            // N.B. explicit run and wait since the test should only continue
+            // if the previous JavaFX access as been finished.
+            if (isFxAnnotation) {
+                FXUtils.runAndWait(testToBeExecuted);
+            } else {
+                testToBeExecuted.run();
+            }
+            final Throwable t = throwable.get();
+            if (t != null) {
+                throw t;
+            }
+        }
+    }
+}
---
+++ subprojects/testfx-junit5/src/test/java/org/testfx/framework/junit5/utils/FXUtilsTests.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2013-2014 SmartBear Software
+ * Copyright 2014-2020 The TestFX Contributors
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the
+ * European Commission - subsequent versions of the EUPL (the "Licence"); You may
+ * not use this work except in compliance with the Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ * http://ec.europa.eu/idabc/eupl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the Licence for the
+ * specific language governing permissions and limitations under the Licence.
+ */
+package org.testfx.framework.junit5.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.framework.junit5.TestFx;
+import org.testfx.framework.junit5.JavaFXInterceptorUtils.SelectiveJavaFxInterceptor;
+
+
+
+/**
+ * Tests {@link  org.testfx.framework.junit5.utils.FXUtils }
+ *
+ * @author RalphSteinhagen
+ *
+ */
+@ExtendWith(ApplicationExtension.class)
+@ExtendWith(SelectiveJavaFxInterceptor.class)
+public class FXUtilsTests {
+    private static final Class<?> clazz = FXUtilsTests.class;
+    private static final Logger LOGGER = LoggerFactory.getLogger(clazz);
+    private static final int WIDTH = 300;
+    private static final int HEIGHT = 200;
+    private Label testLabel;
+
+    @Start
+    public void start(@SuppressWarnings("unused") Stage stage) {
+        // needed only to initialize FX UI Thread infrastructure
+        testLabel = new Label("test label");
+        stage.setScene(new Scene(testLabel, WIDTH, HEIGHT));
+        stage.show();
+    }
+
+    @TestFx
+    public void testWithinFxThread() throws Exception {
+        FXUtils.assertJavaFxThread();
+        FXUtils.keepJavaFxAlive();
+
+        FXUtils.runAndWait(() -> {
+            LOGGER.trace(() -> "execute Runnable in JavaFX thread");
+            FXUtils.assertJavaFxThread();
+        });
+
+        assertTrue(FXUtils.runAndWait(() -> {
+            LOGGER.trace(() -> "execute Supplier in JavaFX thread");
+            FXUtils.assertJavaFxThread();
+            return true;
+        }));
+
+        assertEquals(42.0, FXUtils.runAndWait(42.0, a -> {
+            LOGGER.trace(() -> "execute Function<R,T> in JavaFX thread");
+            FXUtils.assertJavaFxThread();
+            return a;
+        }));
+
+        FXUtils.runFX(() -> {
+            LOGGER.trace(() -> "execute Runnable in runLater in JavaFX thread");
+            FXUtils.assertJavaFxThread();
+        });
+
+        Awaitility.setDefaultPollDelay(100, TimeUnit.MILLISECONDS);
+        Awaitility.pollInSameThread();
+        // FXUtils.waitForFxTicks(testLabel.getScene(), 3);
+        Awaitility.waitAtMost(200, TimeUnit.MILLISECONDS).until(() -> {
+            return FXUtils.waitForFxTicks(testLabel.getScene(), 3);
+        });
+
+        Awaitility.await().atMost(200, TimeUnit.MILLISECONDS).until(() -> {
+            return FXUtils.waitForFxTicks(testLabel.getScene(), 3, 100);
+        });
+
+        // test assertions
+        assertThrows(IllegalStateException.class, () -> FXUtils.runFX(() -> {
+            LOGGER.trace(() -> "execute Runnable with exception in runFX in JavaFX thread");
+            throw new IllegalStateException("should be caught and swallowed by unit-test");
+        }));
+
+        assertThrows(IllegalStateException.class, () -> FXUtils.runAndWait(() -> {
+            LOGGER.trace(() -> "execute Runnable with exception in runAndWait in JavaFX thread");
+            throw new IllegalStateException("should be caught and swallowed by unit-test");
+        }));
+    }
+
+    @Test
+    public void testOutsideFxThread() throws Exception {
+        assertFalse(Platform.isFxApplicationThread());
+
+        assertThrows(IllegalStateException.class, () -> FXUtils.assertJavaFxThread());
+
+        FXUtils.runAndWait(() -> {
+            LOGGER.trace(() -> "execute Runnable in JavaFX thread");
+            FXUtils.assertJavaFxThread();
+        });
+
+        assertTrue(FXUtils.runAndWait(() -> {
+            LOGGER.trace(() -> "execute Supplier in JavaFX thread");
+            FXUtils.assertJavaFxThread();
+            return true;
+        }));
+
+        assertEquals(42.0, FXUtils.runAndWait(42.0, a -> {
+            LOGGER.trace(() -> "execute Function<R,T> in JavaFX thread");
+            FXUtils.assertJavaFxThread();
+            return a;
+        }));
+
+        FXUtils.runFX(() -> {
+            LOGGER.trace(() -> "execute Runnable in runLater in JavaFX thread");
+            FXUtils.assertJavaFxThread();
+        });
+
+        Awaitility.setDefaultPollDelay(100, TimeUnit.MILLISECONDS);
+        Awaitility.pollInSameThread();
+        // FXUtils.waitForFxTicks(testLabel.getScene(), 3);
+        Awaitility.waitAtMost(200, TimeUnit.MILLISECONDS).until(() -> {
+            return FXUtils.waitForFxTicks(testLabel.getScene(), 3);
+        });
+
+        LOGGER.info(() -> "following [FXUtils-thread] 'FXUtils::waitForTicks(..) interrupted by timeout' warning is the normal library behaviour");
+        Awaitility.await().atMost(200, TimeUnit.MILLISECONDS).until(() -> {
+            return FXUtils.waitForFxTicks(testLabel.getScene(), 3, 100);
+        });
+
+        // check for own time-out
+        assertFalse(FXUtils.waitForFxTicks(testLabel.getScene(), 1000, 20));
+
+        // test assertions
+        // N.B. the exception thrown in runLater cannot be forwarded to the calling thread (asynchronicity)
+        // assertThrows(IllegalStateException.class, () -> FXUtils.runFX(() -> {
+        //      LOGGER.atInfo().log("execute Runnable with exception in runFX in JavaFX thread");
+        //      throw new IllegalStateException("should be caught and swallowed by unit-test");
+        // }));
+
+        assertThrows(IllegalStateException.class, () -> FXUtils.runAndWait(() -> {
+            LOGGER.trace(() -> "execute Runnable with exception in runAndWait in JavaFX thread");
+            throw new IllegalStateException("should be caught and swallowed by unit-test");
+        }));
+    }
+}
